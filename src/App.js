9e9c1405d5ec9256084a1560557f2aa9c4953f3b
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-16 02:09:16 -0400

    prog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from "react";
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const [events, setEvents] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  useEffect( () => {
+    getEvents();
+    
+  }, [query]);
+
+  const updateSearch = event =>{
+    setSearch(e.target.value);
+    console.log(search);
+  }
+  
+  const getSearch = event =>{
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  return(
     <div className="App">
-      <div>hi</div>
+      <form className="search-form" onSubmit={getSearch}>
+        <input onChange={updateSearch} className="search-bar" type="text" placeholder="Enter an event or date"/>
+        <button className="search-btn" type="submit">Search</button>
+      </form>
+
+      <div className="events">
+
+      </div>
     </div>
   );
+
 }
 
 export default App;
